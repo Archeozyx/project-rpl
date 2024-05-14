550b502e2b0efa333f073a9193139aa7c1266e44
Archeozyx <radityacahyadi100kun@gmail.com>
2024-05-14 21:50:50 +0700

    hapus navbar wisata, bedo karo srs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,9 +25,6 @@
             <li class="nav-item">
               <a class="nav-link" href="{{ route('wisata') }}">Wisata</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/wisata">Galeri</a>
-            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Akun
