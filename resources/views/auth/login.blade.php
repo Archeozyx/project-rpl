f72f65c365fb85f1d86fcdded974ed180cbe1593
Archeozyx <radityacahyadi100kun@gmail.com>
2024-05-28 22:58:25 +0700

    modified frontend login & register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body>
-   <x-navbar></x-navbar>
-   <x-header>Selamat Datang di Desa Wisata Tingkir Lor</x-header>
+
+<body style="background-color: #e2edff;">
+    <x-navbar></x-navbar>
+    <x-header>Selamat Datang di Desa Wisata Tingkir Lor</x-header>
 
     <div class="container mt-5">
         <h2 class="welcome-text text-center">Selamat Datang di Desa Wisata Tingkir Lor</h2>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mt-5">
             <div class="col-md-4">
                 <div class="login-form">
                     <form method="POST" action="{{ route('login') }}">
@@ -22,15 +24,16 @@
                             <input type="email" class="form-control" id="email" name="email" placeholder="Email">
                         </div>
                         <div class="mb-3">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Kata Sandi">
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="Kata Sandi">
                             <div class="mt-2 text-end">
                                 <a href="#" class="text-decoration-none">Lupa Kata Sandi?</a>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Masuk Sekarang</button>
-                            <div class="mt-2 text-center">
-                                <a href="{{ route('register') }}" class="text-decoration-none">Tidak Ada Akun?</a>
-                            </div>
+                        <div class="mt-2 text-center">
+                            <a href="{{ route('register') }}" class="text-decoration-none">Tidak Ada Akun?</a>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -39,4 +42,5 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
