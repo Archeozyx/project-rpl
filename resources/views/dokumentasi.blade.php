ef692efc1a92d4d4ffa97d5392c4a63b59ee2d39
Archeozyx <radityacahyadi100kun@gmail.com>
2024-06-25 01:19:37 +0700

    update page dokumentasi, search not yet

--- resources/views/dokumentasi.blade.php
+++ resources/views/dokumentasi.blade.php
@@ -21,9 +21,8 @@
 
 <body>
 
+
     <x-navbar></x-navbar>
-
-
     <!-- hero -->
     <div class="container-flex jumbotron"
         style="height: 50vh; margin-top: 13vh; background-image: url(images/bantuan.png); background-size: cover; background-repeat: no-repeat;">
@@ -35,31 +34,38 @@
                 <h5>Beri tahu kami apa masalah mu</h5>
             </div>
             <div class="col-12 d-flex justify-content-center">
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-dark" type="submit">Search</button>
+                <form action="{{ route('dokumentasi') }}" method="GET" id="form-search" class="d-flex" role="search">
+                    <input id="keyword" class="form-control me-2" type="search" name="q" placeholder="Search"
+                        aria-label="Search">
+                    <button class="btn btn-outline-dark" value="Cari" type="submit">Search</button>
                 </form>
             </div>
         </div>
     </div>
 
     <div class="container">
-        <div class="row d-flex justify-content-center mt-5 mb-5">
-            <div class="col-8">
+        <div class="row d-flex justify-content-center mt-4 mb-5">
+            <div class="col-8 text-center mb-4">
+                <h2>FAQ</h2>
+                <hr>
+            </div>
+            <div class="col-md-8">
                 <div class="accordion accordion-flush" id="accordionFlushExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#flush-collapseOne" aria-expanded="false"
                                 aria-controls="flush-collapseOne">
-                                Accordion Item #1
+                                Desa Wisata
                             </button>
                         </h2>
                         <div id="flush-collapseOne" class="accordion-collapse collapse"
                             data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to
-                                demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
-                                body.</div>
+                            <div class="accordion-body" style="color: #555555;">Desa wisata adalah sebuah desa yang
+                                memiliki potensi wisata dan
+                                dikembangkan sebagai destinasi wisata. Desa wisata menawarkan berbagai daya tarik
+                                wisata, seperti wisata alam, wisata budaya, wisata agrowisata, dan wisata buatan
+                                manusia.</div>
                         </div>
                     </div>
                     <div class="accordion-item">
@@ -67,14 +73,14 @@
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#flush-collapseTwo" aria-expanded="false"
                                 aria-controls="flush-collapseTwo">
-                                Accordion Item #2
+                                Pembelian tiket
                             </button>
                         </h2>
                         <div id="flush-collapseTwo" class="accordion-collapse collapse"
                             data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to
-                                demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion
-                                body. Let's imagine this being filled with some actual content.</div>
+                            <div class="accordion-body" style="color: #555555;">Anda harus memiliki akun terlebih dahulu
+                                untuk dapat melakukan
+                                pembelian tiket</div>
                         </div>
                     </div>
                     <div class="accordion-item">
@@ -82,16 +88,75 @@
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#flush-collapseThree" aria-expanded="false"
                                 aria-controls="flush-collapseThree">
-                                Accordion Item #3
+                                Pembuatan Akun
                             </button>
                         </h2>
                         <div id="flush-collapseThree" class="accordion-collapse collapse"
                             data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to
-                                demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion
-                                body. Nothing more exciting happening here in terms of content, but just filling up the
-                                space to make it look, at least at first glance, a bit more representative of how this
-                                would look in a real-world application.</div>
+                            <div class="accordion-body" style="color: #555555;">Anda dapat melakukan registrasi dengan
+                                menggunakan akun email
+                                anda. Setelah itu anda dapat login dengan akun email yang telah anda registrasikan</div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseFour" aria-expanded="false"
+                                aria-controls="flush-collapseFour">
+                                Wisata Gowes
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFour" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body" style="color: #555555;">Pengunjung akan diajak bersepeda
+                                mengelilingi
+                                desa Tingkir Lor Cengek dengan pemandangan yang indah.</div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseFive" aria-expanded="false"
+                                aria-controls="flush-collapseFive">
+                                Wisata Religi
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFive" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body" style="color: #555555;">Pengunjung dipersilahkan untuk ziarah ke
+                                makam Mbah Abdul Wahid yang berada di Tingkir Lor. Beliau merupakan salah satu keluarga
+                                KH
+                                Abdurrahman Wahid yang merupakan Presiden ke-4 Republik Indonesia</div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseSix" aria-expanded="false"
+                                aria-controls="flush-collapseSix">
+                                Wisata Edukasi Coklat Tempe
+                            </button>
+                        </h2>
+                        <div id="flush-collapseSix" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body" style="color: #555555;">Pengunjung akan diedukasi tentang
+                                bagaimana proses pembuatan coklat tempe, pengunjung juga berkesempatan untuk membuat
+                                coklat tempe bersama-sama</div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapseSeven" aria-expanded="false"
+                                aria-controls="flush-collapseSeven">
+                                Wisata Gamelan
+                            </button>
+                        </h2>
+                        <div id="flush-collapseSeven" class="accordion-collapse collapse"
+                            data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body" style="color: #555555;">Pengunjung akan diajak untuk belajar dan
+                                memainkan alat musik tradisional gamelan bersama para ahli, serta memahami sejarah dan
+                                filosofi yang mendalam di balik setiap bunyi dan nada.</div>
                         </div>
                     </div>
                 </div>
@@ -99,13 +164,117 @@
         </div>
     </div>
 
+    <div class="container" id="content">
+        <div class="row">
+            <div class="col text-center mb-3 ms-5">
+                <h3 id="login-page">Panduan Penggunaan Website</h3>
+                <hr>
+
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8 mb-5">
+                <h5>Halaman Login</h5>
+                <ul>
+                    <li>
+                        Halaman login digunakan oleh pengguna dan admin untuk masuk ke sistem.
+                    </li>
+                    <li>
+                        Pengguna memasukkan kredensial login mereka pada form yang disediakan.
+                    </li>
+                </ul>
+                <h5>Halaman Utama</h5>
+                <ul>
+                    <li>
+                        Halaman utama menampilkan informasi umum tentang Desa Tingkir Lor, Cengek.
+                    </li>
+                    <li>
+                        Terdapat foto-foto dari desa wisata untuk memberikan gambaran kepada pengunjung.
+                    </li>
+                    <li>
+                        Navigasi menu utama terletak di bagian atas halaman untuk memudahkan pengunjung mengakses
+                        halaman lain.
+                    </li>
+                </ul>
+                <h5>Halaman Wisata</h5>
+                <ul>
+                    <li>
+                        Halaman ini berisi informasi detail tentang wisata yang ada di Desa Cengek.
+                    </li>
+                    <li>
+                        Setiap objek wisata dijelaskan secara singkat beserta foto-foto terkait.
+                    </li>
+                    <li>
+                        Pengunjung dapat menjelajahi berbagai pilihan wisata yang ditawarkan.
+                    </li>
+                </ul>
+                <h5>Halaman Pemesanan Tiket</h5>
+                <ul>
+                    <li>
+                        Halaman pemesanan tiket menampilkan daftar wisata yang tersedia.
+                    </li>
+                    <li>
+                        Pengunjung dapat memilih paket wisata yang diinginkan.
+                    </li>
+                    <li>
+                        Terdapat form pemesanan tiket yang harus diisi oleh pengunjung meliputi
+                        Email,
+                        Nomor HP,
+                        Nama Wisatawan/Ketua Rombongan,
+                        Usia,
+                        Tanggal Kedatangan,
+                        Pilihan Paket Wisata, dan
+                        Jumlah Rombongan.
+                    </li>
+                </ul>
+                <h5>Halaman Pengelola (khusus admin)</h5>
+                <ul>
+                    <li>
+                        Setelah login, pada link web http://127.0.0.1:8000/ tambahkan admin di akhir menjadi
+                        http://127.0.0.1:8000//admin, setelah itu akan masuk ke halaman admin
+                    </li>
+                    <li>
+                        Admin dapat mengelola informasi pada halaman utama, halaman wisata, dan halaman pemesanan tiket.
+                    </li>
+                    <li>
+                        Terdapat fitur laporan pemesanan tiket yang menampilkan data pemesanan dalam bentuk tabel
+                    </li>
+                    <li>
+                        Admin dapat melakukan tindakan seperti mengedit, menyimpan, menghapus, dan mengunduh data
+                        pemesanan
+                    </li>
+                </ul>
+                <h5>Laporan Pemesanan Tiket</h5>
+                <ul>
+                    <li>
+                        Laporan pemesanan tiket menampilkan data pemesanan dalam format tabel.
+                    </li>
+                    <li>
+                        Informasi yang ditampilkan meliputi
+                        Tanggal pemesanan,
+                        Email pemesan,
+                        Nomor HP pemesan,
+                        Nama pemesan,
+                        Tanggal kedatangan,
+                        Pilihan paket,
+                        Jumlah orang, dan
+                        Status pemesanan.
+                    </li>
+                    <li>
+                        Admin dapat melakukan tindakan seperti mengedit, menyimpan, menghapus, dan mengunduh data
+                        pemesanan.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
 
 
     <!-- footer -->
     <div class="container-flex pb-5 text-white text-center" style="background-color: #273044;">
-        <div class="col-md-12">
+        <div class="col-md-12 pt-5">
             <h4 class="display-7">Desa Wisata Tingkir Lor</h4>
             <a href="https://www.instagram.com/desawisata_tingkirlor/" class="text-white fw-bold">Instagram <i
                     class="bi bi-instagram"></i></a>
